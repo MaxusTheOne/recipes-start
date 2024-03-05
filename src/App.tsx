8231fb867d7e07cd26c55b01e42a930f9c16fc04
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2024-03-05 13:15:00 +0100

    Part 1 done

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Login from "./security/Login";
 import Layout from "./Layout";
 import Home from "./Home";
 import "./App.css";
+import RecipesLayout from "./recipes/RecipesLayout";
 
 export default function App() {
   //const auth = useAuth();
@@ -16,13 +17,14 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories/" element={<Categories />} />
-        <Route path="/recipes">
+        <Route path="/recipes" element= {<RecipesLayout/>}>
           <Route index element={<Recipes />} />
           <Route path=":id" element={<Recipe />} />
         </Route>
         <Route path="/add" element={<RecipeForm />} />
         <Route path="/login" element={<Login />} />
         {/* <Route path="/logout" element={<Logout />} /> */}
+        <Route path="*" element={<h2>Not Found</h2>} />
       </Routes>
     </Layout>
   );
